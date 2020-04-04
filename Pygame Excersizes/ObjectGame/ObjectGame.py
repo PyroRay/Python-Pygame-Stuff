eb30a32bfe2d41c839ad76eaf9bef36567c55fb3
Ray Peng <pengray2015@gmail.com>
2020-04-04 11:09:51 -0700

    Update ObjectGame.py
    Updated ObjGame

--- Pygame Excersizes/ObjectGame/ObjectGame.py
+++ Pygame Excersizes/ObjectGame/ObjectGame.py
@@ -1,7 +1,11 @@
+
+#region Imports
 import pygame, sys, random, math
 from pygame.locals import *
 pygame.init()
+#endregion
 
+#region Defining Variables
 windowSurfaceObj = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Object Game')
 clrBlack = pygame.Color(0, 0, 0)
@@ -12,78 +16,88 @@ left_pressed = False
 right_pressed = False
 up_pressed = False
 down_pressed = False
+delay = 20 # How many pixels off screen the robot will be when it wraps around to the bottom from top
 timer = 0
-directions = ("slowleft", "slowright")
-numRobots = 3
+numRobots = 5
 screendim = pygame.display.get_surface().get_size()
-# badrobot = ()
-# badrobot1direction = ""
-# badrobot2direction = ""
+#endregion
 
+#region Classes
+
+class vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def magnitude(self):
+        return math.sqrt(x**2 + y**2)
 
 class robot:
-    def __init__(self, colorset, xset, yset):
+    def __init__(self, colorset, xset, yset, dirx=0, diry=0):
         self.radius = 15
         self.set(colorset,xset,yset)
+        self.direction = vector(dirx, diry)
 
-    def move(self, direction):
-        if direction == "up":
-            self.y -= 3
-        elif direction == "down":
-            self.y += 3
-        elif direction == "left":
-            self.x -= 3
-        elif direction == "right":
-            self.x += 3
-        elif direction == "slowup":
-            self.y -= 1
-        elif direction == "slowdown":
-            self.y += 1
-        elif direction == "slowleft":
-            self.x -= 1
-        elif direction == "slowright":
-            self.x += 1
+    def setdirection(self,dirx=None,diry=None):
+        if(dirx != None):
+            self.direction.x = dirx
+        if(diry != None):
+            self.direction.y = diry
+
+    def move(self, dirx=None, diry=None):
+        if(dirx == None):
+            self.x += self.direction.x
+        else:
+            self.x += dirx
+        if(diry == None):
+            self.y += self.direction.y
+        else:
+            self.y += diry
 
     def draw(self):
         pygame.draw.circle(windowSurfaceObj, (self.color), (self.x, self.y), self.radius)
 
-    def set(self, colorset, xset, yset):
+    def set(self, colorset, xset, yset,dirx=0,diry=0):
         self.color = colorset
         self.x = xset
         self.y = yset
+        self.direction = vector(dirx,diry)
 
     def setXY(self, xset, yset):
         self.set(self.color, xset, yset)
+#endregion
 
-def badrobotmovement(badrobot, timer, badrobotdirection, directions):
-    if timer % 120 == 0:
-        badrobotdirection = random.choice(directions)
+#region Functions
+# def badrobotmovement(badrobot, timer, badrobotdirection, directions):
+#     if timer % 120 == 0:
+#         badrobotdirection = random.choice(directions)
+#     else:
+#         if badrobot.x > screendim[0]:
+#             badrobotdirection = "slowright"
+#         elif badrobot.x < 0:
+#             badrobotdirection = "slowleft"
     
-    return badrobotdirection
+#     return badrobotdirection
+#endregion
 
+#region Robot Spawning
 playerbot = robot(clrWhite, screendim[0]//2, screendim[1]//2)
 
 robots = []
 
-
+#spawn robots
 while numRobots > 0:
-    robots.append(robot(clrRed, screendim[1] + 20, random.randint(0, screendim[0])))
+    # print(screendim[1])
+    robots.append(robot(clrRed, random.randint(0, screendim[0]), screendim[1] + 20,random.randint(-1,1),-1))
     numRobots-= 1
-    
-print(len(robots))
 
-# badrobot1 = robot()
-# badrobot1.color = clrRed
-# badrobot1.x = pygame.display.get_surface().get_size()[0]//3
-# badrobot1.y = pygame.display.get_surface().get_size()[1]//2
+#endregion
 
-# badrobot2 = robot()
-# badrobot2.color = clrRed
-# badrobot2.x = pygame.display.get_surface().get_size()[0]//3
-# badrobot2.y = pygame.display.get_surface().get_size()[1]//2
-
+#region Main Game Loop
 while True:
     windowSurfaceObj.fill(clrBlack)	
+
+    #region Events
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -114,33 +128,41 @@ while True:
             elif event.key == pygame.K_d:
                 # print("\'d\' key was let go")
                 right_pressed = False
+    #endregion
 
-    if timer % 180 == 0:
-        print("hello")
-
+    #region Player Direction
     if up_pressed:
-        playerbot.move("up")
+        playerbot.move(0,-3)
     if down_pressed:
-        playerbot.move("down")
+        playerbot.move(0,3)
     if left_pressed:
-        playerbot.move("left")
+        playerbot.move(-3,0)
     if right_pressed:
-        playerbot.move("right")
+        playerbot.move(3,0)
+    #endregion
 
-    # if timer > 120:
-    #     timer = 0
+    #region Robots Direction
+    for i in range(len(robots)):
+        # print(i)
+        if timer % 120 == 0:
+            robots[i].setdirection(random.randint(-1,1),-1)
+        elif(robots[i].x > screendim[0]):
+            robots[i].setdirection(-1)
+        elif(robots[i].x < 0):
+            robots[i].setdirection(1)
+        if(robots[i].y <= 0):
+            robots[i].y = screendim[1] + delay
+        robots[i].move()
+        robots[i].draw()
+    #endregion
 
-    # badrobot1direction = badrobotmovement(badrobot1, timer, badrobot1direction, directions)
-    # badrobot1.move(badrobot1direction)
-    # badrobot1.move("slowup")
-    # badrobot2direction = badrobotmovement(badrobot2, timer, badrobot2direction, directions)
-    # badrobot2.move(badrobot2direction)
-    # badrobot2.move("slowup")
-    
     playerbot.draw()
-    # badrobot1.draw()
-    # badrobot2.draw()
-    timer += 1
 
+
+    #region Ticks
+    timer += 1
     pygame.display.update()
-    fpsClock.tick(60)
+    fpsClock.tick(120)
+    #endregion
+
+#endregion
