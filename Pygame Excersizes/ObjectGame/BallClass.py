81b8256624a40ac5873f0a8ace42d4413eb351fb
Ray Peng <pengray2015@gmail.com>
2020-04-18 13:10:18 -0700

    Angle Experimentation

--- Pygame Excersizes/ObjectGame/BallClass.py
+++ Pygame Excersizes/ObjectGame/BallClass.py
@@ -1,7 +1,7 @@
-
 #region Imports
 import pygame, sys, random, math
 from pygame.locals import *
+from pygame.math import Vector2
 pygame.init()
 #endregion
 
@@ -24,20 +24,20 @@ screendim = pygame.display.get_surface().get_size()
 
 #region Classes
 
-class vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class vector:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
     
-    def magnitude(self):
-        return math.sqrt(x**2 + y**2)
+#     def magnitude(self):
+#         return math.sqrt(x**2 + y**2)
 
 class robot:
     def __init__(self, speed, colorset, xset, yset, dirx=0, diry=0):
         self.image = pygame.image.load('squirrel.png')
         self.radius = 15 #basically the size
         self.set(speed,colorset,xset,yset)
-        self.direction = vector(dirx, diry)
+        self.direction = Vector2(dirx, diry)
         self.speed = speed
 
     def setdirection(self,dirx=None,diry=None):
@@ -49,6 +49,7 @@ class robot:
     def move(self, dirx=None, diry=None):
         if(dirx == None):
             self.x += self.direction.x
+            
         else:
             self.x += dirx
         if(diry == None):
@@ -57,14 +58,14 @@ class robot:
             self.y += diry
 
     def draw(self):
-        pygame.draw.circle(windowSurfaceObj, (self.color), (self.x, self.y), self.radius)
+        pygame.draw.circle(windowSurfaceObj, (self.color), (int(self.x), int(self.y)), self.radius)
 
     def set(self, speed, colorset, xset, yset,dirx=0,diry=0):
         self.speed = speed
         self.color = colorset
         self.x = xset
         self.y = yset
-        self.direction = vector(dirx,diry)
+        self.direction = Vector2(dirx,diry)
 
     def setXY(self, xset, yset):
         self.set(self.color, xset, yset)
@@ -122,25 +123,31 @@ while True:
     #region Player Direction
 
     if up_pressed:
-        playerbot.move(0,-playerbot.speed)
+        playerbot.setdirection(None ,playerbot.direction.y - playerbot.speed)
+        #playerbot.move(0,-playerbot.speed)
     if down_pressed:
-        playerbot.move(0,playerbot.speed)
+        playerbot.setdirection(None ,playerbot.direction.y + playerbot.speed)
+        #playerbot.move(0,playerbot.speed)
     if left_pressed:
-        playerbot.move(-playerbot.speed,0)
+        playerbot.setdirection(playerbot.direction.x - playerbot.speed,None)
+        #playerbot.move(-playerbot.speed,0)
     if right_pressed:
-        playerbot.move(playerbot.speed,0)
-    if playerbot.x < 0:
-        right_pressed = True
-        left_pressed = False
-    if playerbot.x > screendim[0]:
-        right_pressed = False
-        left_pressed = True
-    if playerbot.y < 0:
-        up_pressed = False
-        down_pressed = True
-    if playerbot.y > screendim[1]:
-        up_pressed = True
-        down_pressed = False
+        playerbot.setdirection(playerbot.direction.x + playerbot.speed,None)
+        #playerbot.move(playerbot.speed,0)
+    if playerbot.x < 0 or playerbot.x > screendim[0]:
+        playerbot.setdirection(-playerbot.direction.x,None)
+        #right_pressed = True
+        #left_pressed = False
+    #if playerbot.x > screendim[0]:
+        #right_pressed = False
+        #left_pressed = True
+    if playerbot.y < 0 or playerbot.y > screendim[1]:
+        playerbot.setdirection(None,-playerbot.direction.y)
+        #up_pressed = False
+        #down_pressed = True
+    #if playerbot.y > screendim[1]:
+        #up_pressed = True
+        #down_pressed = False
         
     #endregion
 
@@ -158,25 +165,38 @@ while True:
         if(robots[i].y >= screendim[1] + 2*(delay)):
             robots[i].y = 0 - delay
         #region collision
-        if math.sqrt(abs(robots[i].x - playerbot.x)**2 + abs(robots[i].y - playerbot.y)**2) < (playerbot.radius + robots[i].radius):
-            if robots[i].x > playerbot.x:
-                robots[i].setdirection(robots[i].speed)
-                right_pressed = False
-                left_pressed = True
-            elif robots[i].x < playerbot.x:
-                robots[i].setdirection(-robots[i].speed)
-                right_pressed = True
-                left_pressed = False
+        impact = Vector2((playerbot.x- robots[i].x),(playerbot.y-robots[i].y)) # the vector pointing from bad ball to player 
+        if impact.magnitude() <= (playerbot.radius + robots[i].radius): # then, we have impact
+            # new direction vector for player = ndir_player 
+            # ndir_player = dir_player + magnitude(dir_enemy)*cosɵ*impact_unit
+            impact_unit = impact / impact.magnitude()
+            cosTheta = robots[i].direction.dot(impact_unit) / robots[i].direction.magnitude()
+            playerbot.direction = playerbot.direction +robots[i].direction.magnitude()*cosTheta*impact_unit
+            robots[i].direction = robots[i].direction + playerbot.direction.magnitude()*cosTheta*(-impact_unit)
+            up_pressed = False
+            left_pressed = False
+            right_pressed = False
+            down_pressed = False
+            # if robots[i].x > playerbot.x: # bad ball is to the right of player
+            #     robots[i].setdirection(robots[i].speed)
+            #     right_pressed = False
+            #     left_pressed = True
+            # elif robots[i].x < playerbot.x:
+            #     robots[i].setdirection(-robots[i].speed)
+            #     right_pressed = True
+            #     left_pressed = False
             
-            if robots[i].y > playerbot.y:
-                robots[i].setdirection(None, robots[i].speed)
-                down_pressed = False
-                up_pressed = True
-            elif robots[i].y < playerbot.y:
-                robots[i].setdirection(None, -robots[i].speed)
-                down_pressed = True
-                up_pressed = False
+            # if robots[i].y > playerbot.y:
+            #     robots[i].setdirection(None, robots[i].speed)
+            #     down_pressed = False
+            #     up_pressed = True
+            # elif robots[i].y < playerbot.y:
+            #     robots[i].setdirection(None, -robots[i].speed)
+            #     down_pressed = True
+            #     up_pressed = False
         #endregion
+        
+        playerbot.move()
         robots[i].move()
         robots[i].draw()
     #endregion
@@ -190,7 +210,7 @@ while True:
     #region Ticks
     timer += 1
     pygame.display.update()
-    fpsClock.tick(60)
+    fpsClock.tick(20)
     #endregion
 
 #endregion
