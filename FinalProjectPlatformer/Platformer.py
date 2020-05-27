84d4990fe247a76ace6a4777ea6d097779b03c5e
Ray Peng <ygnihTtaC@users.noreply.github.com>
2020-05-27 10:24:28 -0700

    Update Platformer.py

--- FinalProjectPlatformer/Platformer.py
+++ FinalProjectPlatformer/Platformer.py
@@ -21,7 +21,7 @@ screendim = pygame.display.get_surface().get_size()
 runGame = True
 
 class player:
-    def __init__(self, speed, color, height, width, xset, yset):
+    def __init__(self, speed, color, height, width, xset, yset, falling = False):
         # self.image = pygame.image.load('squirrel.png')
         self.radius = 15 #basically the size
         self.direction = ""
@@ -30,6 +30,7 @@ class player:
         self.size = (width, height)
         self.x = xset
         self.y = yset
+        self.falling = falling
 
     def draw(self):
         pygame.draw.rect(windowSurfaceObj, (self.color), (self.x - self.size[0]//2, self.y - self.size[1]//2, self.size[0], self.size[1]))
@@ -83,10 +84,14 @@ while runGame:
 
     if player1.y + player1.size[1] >= screendim[1]-20: # player is on the ground
         # print("ground")
-        # player1.move() # player is not moving  
+        # player1.move() # player is not moving
+        player1.falling = False
         if up_pressed:
-            player1.move(0)
-    else:
+            player1.move(0, -10)
+        else:
+            player1.falling = True
+
+    if player1.falling:
         player1.move(0, 5)
 
     player1.move()
